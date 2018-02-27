cd111b4d5ef063d8833777569796ee679b2630b5
sarahchang2013 <sarah_sarah99@hotmail.com>
2018-02-27 12:59:57 +0800

    add comment to warn not to use readFileSync

--- 03_FileIO/app.js
+++ 03_FileIO/app.js
@@ -1,8 +1,10 @@
 ﻿var fs = require('fs');
-
+//never call readFileSync in a node express/webserver
+//node is designed to work async
 var contents = fs.readFileSync('package.json').toString();
 console.log(contents);
 
 fs.readFile('package.json', function (err, buf) {
     console.log(buf.toString());
+    console.log("Async readFile() called.")
 });

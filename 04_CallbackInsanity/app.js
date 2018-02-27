618f620f7ef9f5eec446feee90474f0513ffc0ea
sarahchang2013 <sarah_sarah99@hotmail.com>
2018-02-27 15:12:05 +0800

    use fileIndex in forEach()

--- 04_CallbackInsanity/app.js
+++ 04_CallbackInsanity/app.js
@@ -4,11 +4,14 @@ fs.readdir('.', function (err, files) {
     if (err) {
         console.log('Error finding files: ' + err)
     } else {
+        //Definition of forEach in Javascript:
+        //arr.forEach(function callback(currentValue[, index[, array]])
         files.forEach(function (filename, fileIndex) {
             fs.readFile(filename, function (err, buf) {
                 if (err) {
                     console.log('Error reading file:' + err);   
                 } else {
+                    console.log("File" + fileIndex + ":")
                     console.log(buf.toString());
                 }
             });

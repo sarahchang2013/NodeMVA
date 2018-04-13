69146e1a446e132ddcba6799ac166566723395e2
sarahchang2013 <sarah_sarah99@hotmail.com>
2018-04-13 17:05:21 +0800

    more comments on 07_basicexpress

--- 07_BasicExpress/routes/index.js
+++ 07_BasicExpress/routes/index.js
@@ -2,7 +2,8 @@
 /*
  * GET home page.
  */
-
+//A function called index is defined and called when '/' is requested
+//see the app.get() in app.js
 exports.index = function (req, res) {
     res.render('index', { title: 'Express' });
 };
--- 07_BasicExpress/routes/user.js
+++ 07_BasicExpress/routes/user.js
@@ -2,7 +2,8 @@
 /*
  * GET users listing.
  */
-
+//A function called list is defined and called when '/users' is requested
+// see the app.get() in app.js
 exports.list = function (req, res) {
     res.send("respond with a resource");
 };
